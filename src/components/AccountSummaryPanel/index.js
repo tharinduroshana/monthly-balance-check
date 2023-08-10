ca786035a93ccb9f8e10227500e81df3b12a606a
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-08-10 19:35:14 +0300

    Modified Account summary to display values

--- src/components/AccountSummaryPanel/index.js
+++ src/components/AccountSummaryPanel/index.js
@@ -1,30 +1,30 @@
 import "./AccountSummaryPanel.css";
 
-const AccountSummaryPanel = () => {
+const AccountSummaryPanel = ({ monthlyBalance }) => {
   return (
-      <div className="flex-column font-size-small">
-          <div className="flex-row">
-              <div className="flex-row width-50 space-between">
-                  <div className="flex-1">Account Number:</div>
-                  <div className="flex-1">1233 2344 5675</div>
-              </div>
-              <div className="flex-row width-50 space-between">
-                  <div className="flex-1">Monthly Balance (EUR):</div>
-                  <div className="flex-1">23,465.54</div>
-              </div>
-          </div>
-          <div className="flex-row">
-              <div className="flex-row width-50 space-between">
-                  <div className="flex-1">Account Type:</div>
-                  <div className="flex-1">Savings</div>
-              </div>
-              <div className="flex-row width-50 space-between">
-                  <div className="flex-1">Floating Balance (EUR):</div>
-                  <div className="flex-1">0,00</div>
-              </div>
-          </div>
+    <div className="flex-column font-size-small">
+      <div className="flex-row">
+        <div className="flex-row width-50 space-between">
+          <div className="flex-1 font-weight-bolder">Account Number:</div>
+          <div className="flex-1">1233 2344 5675</div>
+        </div>
+        <div className="flex-row width-50 space-between">
+          <div className="flex-1 font-weight-bolder">Monthly Balance (€):</div>
+          <div className="flex-1">{monthlyBalance.toFixed(2)}</div>
+        </div>
       </div>
+      <div className="flex-row">
+        <div className="flex-row width-50 space-between">
+          <div className="flex-1 font-weight-bolder">Account Type:</div>
+          <div className="flex-1">Savings</div>
+        </div>
+        <div className="flex-row width-50 space-between">
+          <div className="flex-1 font-weight-bolder">Floating Balance (€):</div>
+          <div className="flex-1">0,00</div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default AccountSummaryPanel;
