0899d8a17c324a6c493fd2c054ec82df471a9d9a
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-08-10 19:35:54 +0300

    Added a new util function to use the fetched transactions to calculate cumulative balance

--- src/util/MockAPI.js
+++ src/util/MockAPI.js
@@ -1,13 +1,23 @@
-import json from '../data/transactions.json';
+import json from "../data/transactions.json";
 
 /*
-* The following function will mimic the API call to the server. In order to set a buffer time, setTimeout function
-* was called and returned a Promise.
-* */
+ * The following function will mimic the API call to the server. In order to set a buffer time, setTimeout function
+ * was called and returned a Promise.
+ * */
 export const getTransactionsMockAPI = async () => {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve(json);
     }, 1000);
-  })
-}
+  });
+};
+
+export const getTransactionsWithBalance = async () => {
+  const transactions = await getTransactionsMockAPI();
+  let accumulativeBalance = 0;
+  const newTransactions = transactions.map((tx) => {
+    accumulativeBalance += tx.amount;
+    return { ...tx, balance: accumulativeBalance.toFixed(2) };
+  });
+  return { transactions: newTransactions, monthlyBalance: accumulativeBalance };
+};
