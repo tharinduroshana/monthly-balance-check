6646ced5bd6c222998fbdf55729559aff09ba24f
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-08-10 19:36:55 +0300

    Added the functionality of displaying transaction data

--- src/components/StatementTable/index.js
+++ src/components/StatementTable/index.js
@@ -1,55 +1,61 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import RemoveCircleRoundedIcon from "@mui/icons-material/RemoveCircleRounded";
 
-function createData(id, date, description, amount, balance) {
-    return { id, date, description, amount, balance };
-}
-
-//Dummy data
-const rows = [
-    createData('1', 159, 6.0, 24, 4.0),
-    createData('2', 237, 9.0, 37, 4.3),
-    createData('3', 262, 16.0, 24, 6.0),
-    createData('4', 305, 3.7, 67, 4.3),
-    createData('5', 356, 16.0, 49, 3.9),
-];
-
-const StatementTable = () => {
+const StatementTable = ({ transactions }) => {
   return (
-      <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                  <TableRow>
-                      <TableCell>ID</TableCell>
-                      <TableCell align="right">Date</TableCell>
-                      <TableCell align="right">Description</TableCell>
-                      <TableCell align="right">Amount</TableCell>
-                      <TableCell align="right">Balance</TableCell>
-                  </TableRow>
-              </TableHead>
-              <TableBody>
-                  {rows.map((row) => (
-                      <TableRow
-                          key={row.name}
-                      >
-                          <TableCell component="th" scope="row">
-                              {row.id}
-                          </TableCell>
-                          <TableCell align="right">{row.date}</TableCell>
-                          <TableCell align="right">{row.description}</TableCell>
-                          <TableCell align="right">{row.amount}</TableCell>
-                          <TableCell align="right">{row.balance}</TableCell>
-                      </TableRow>
-                  ))}
-              </TableBody>
-          </Table>
-      </TableContainer>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ fontWeight: "bolder" }}>ID</TableCell>
+            <TableCell sx={{ fontWeight: "bolder" }}>Date</TableCell>
+            <TableCell sx={{ fontWeight: "bolder" }}>Description</TableCell>
+            <TableCell sx={{ fontWeight: "bolder" }} align="right">
+              Amount (€)
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bolder" }} align="right">
+              Balance (€)
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(transactions || []).map((transaction) => (
+            <TableRow key={transaction.id}>
+              <TableCell component="th" scope="row">
+                {transaction.id}
+              </TableCell>
+              <TableCell>{transaction.date}</TableCell>
+              <TableCell>{transaction.description}</TableCell>
+              <TableCell align="right">
+                {Math.abs(transaction.amount).toFixed(2)}
+                {transaction.amount > 0 ? (
+                  <AddCircleRoundedIcon
+                    sx={{ verticalAlign: "bottom", marginLeft: "5px" }}
+                    color="success"
+                    fontSize="small"
+                  />
+                ) : (
+                  <RemoveCircleRoundedIcon
+                    sx={{ verticalAlign: "bottom", marginLeft: "5px" }}
+                    color="error"
+                    fontSize="small"
+                  />
+                )}
+              </TableCell>
+              <TableCell align="right">{transaction.balance}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-}
+};
 
 export default StatementTable;
--- src/screens/ViewMonthlyStatement/index.js
+++ src/screens/ViewMonthlyStatement/index.js
@@ -1,21 +1,41 @@
 import Container from "@mui/material/Container";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import StatementTable from "../../components/StatementTable";
 import AccountSummaryPanel from "../../components/AccountSummaryPanel";
+import { getTransactionsWithBalance } from "../../util/MockAPI";
+import { useEffect, useState } from "react";
+import LoadingScreen from "../../components/LoadingScreen";
 
 const ViewMonthlyStatement = () => {
+  const [transactions, setTransactions] = useState(null);
+  const [monthlyBalance, setMonthlyBalance] = useState(0);
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
+  const getTransactions = async () => {
+    const result = await getTransactionsWithBalance();
+    setTransactions(result.transactions);
+    setMonthlyBalance(result.monthlyBalance);
+  };
+
   return (
     <Container sx={{ "margin-top": "15px" }} maxWidth={false}>
-        <Card>
-            <CardContent>
-                <Typography variant="subtitle1" component="div">
-                    <AccountSummaryPanel />
-                </Typography>
-                <StatementTable />
-            </CardContent>
-        </Card>
+      <Card>
+        <CardContent>
+          <Typography variant="subtitle1" component="div">
+            <AccountSummaryPanel monthlyBalance={monthlyBalance} />
+          </Typography>
+          {transactions ? (
+            <StatementTable transactions={transactions} />
+          ) : (
+            <LoadingScreen openSpinner={!transactions} />
+          )}
+        </CardContent>
+      </Card>
     </Container>
   );
 };
